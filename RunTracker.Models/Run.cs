caaa15b63a875cc72fa1009c3c2e533b4972e56a
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2025-08-08 09:17:23 +0100

    Start of the models project

---
+++ RunTracker.Models/Run.cs
@@ -0,0 +1,11 @@
+﻿namespace RunTracker.Models;
+
+public class Run
+{
+    public int AverageHeartRate { get; init; }
+    public TimeSpan AveragePace { get; init; }
+    public float DistanceKm { get; init; }
+    public TimeSpan Duration { get; init; }
+    public Guid Id { get; init; }
+    public DateTime StartTime { get; init; }
+}
