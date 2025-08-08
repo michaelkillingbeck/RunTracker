2f355f1acf82bc98206c299a827453745b4f2769
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2025-08-08 15:27:09 +0100

    Added basic project to start parsing FIT files.

---
+++ RunTracker.FitParser.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using Dynastream.Fit;
+
+var decoder = new Decode();
+var messages = new List<Mesg>();
+
+int messagesCount = 0;
+
+decoder.MesgEvent += (sender, e) =>
+{
+    messagesCount++;
+    messages.Add(e.mesg);
+};
+
+using var fitStream = new FileStream("FITFiles\\sample.fit", FileMode.Open, FileAccess.Read);
+if (decoder.IsFIT(fitStream) == false)
+{
+    throw new InvalidOperationException("Not a valid FIT file");
+}
+
+decoder.Read(fitStream);
+
+Console.WriteLine($"{messagesCount} messages received");
+Console.WriteLine();
+
+var messageTypes = messages
+    .GroupBy(message => message.Name)
+    .OrderByDescending(group => group.Count())
+    .ToList();
+
+foreach (var messageGroup in messageTypes)
+{
+    Console.WriteLine($"{messageGroup.Key}: {messageGroup.Count()}");
+}
+
+Console.ReadKey();
